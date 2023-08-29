3b47b05d5b08975a05d7c7c7338c0531de0cc10f
011Pratyush <105581109+011Pratyush@users.noreply.github.com>
2023-08-29 21:40:22 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,70 @@
+
+import './App.css';
+import About from './Components/About';
+import Navbar from './Components/Navbar';
+import TextForm from './Components/TextForm';
+import React, { useState } from 'react';
+import Alert from './Components/Alert';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  // Link
+} from "react-router-dom";
+
+function App() {
+  // whether dark mode is enabled or not 
+  const [mode, setMode] = useState('light');
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type
+    })
+    setTimeout(() => {
+      setAlert(null);
+    }, 2000);
+  }
+  const toggleMode = () => {
+    if (mode === 'light') {
+      setMode('dark');
+      document.body.style.backgroundColor = '#403838';
+      document.body.style.color = 'white';
+      showAlert("Dark mode has been enabled", "success");
+      document.title = 'TextUtils - Dark Mode';
+    }
+    else {
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+      document.body.style.color = 'black';
+      showAlert("Light mode has been enabled", "success");
+      document.title = 'TextUtils - Light Mode';
+    }
+  }
+
+  return (
+    <>
+
+      {/* <Navbar title="TextUtils" aboutText="About"/> */}
+      <BrowserRouter>
+        <Navbar title="textUtils" mode={mode} toggleMode={toggleMode} />
+        <Alert alert={alert} />
+        <div className="container my-3">
+          <Routes>
+            {/* <Route path="/" element={<Layout />}> */}
+            <Route exact path="/about" element={<About />}>
+
+            </Route>
+            <Route exact path="/" element={<TextForm showAlert={showAlert} heading="Enter the text" mode={mode} />}>
+              {/* <TextForm showAlert={showAlert} heading ="Enter the text"mode={mode}/> */}
+            </Route>
+          </Routes>
+          {/* <About/> */}
+        </div>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
